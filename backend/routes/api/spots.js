4f8136bd54032365916de6af0b068d653ee4e94a
Justin Hall <justinhall.jnh@gmail.com>
2023-12-01 13:57:50 +0900

    Implement endpoint: Get all Spots owned by the Current User

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { Spot, SpotImage, Review, User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
+// Get all Spots
 router.get('/', async (req, res, next) => {
   let spots = await Spot.findAll({include: [{model: SpotImage}, {model: Review}]});
   spots = {Spots: JSON.parse(JSON.stringify(spots))};
@@ -23,6 +25,19 @@ router.get('/', async (req, res, next) => {
   res.json(spots);
 });
 
+// Get all Spots owned by the Current User
+router.get('/current', requireAuth, async (req, res, next) => {
+  let ownedSpots = await Spot.findAll({
+    where: {ownerId: req.user.id},
+    include: [{model: SpotImage}, {model: Review}]
+  });
+
+  ownedSpots = formatSpots(ownedSpots);
+
+  res.json(ownedSpots);
+});
+
+// Get details of a Spot from an id
 router.get('/:spotId', async (req, res, next) => {
   let spot = await Spot.findOne({
     where: {id: req.params.spotId},
@@ -46,5 +61,26 @@ router.get('/:spotId', async (req, res, next) => {
 
   res.json(spot);
 });
+
+// Utility function
+function formatSpots(spots) {
+  spots = {Spots: JSON.parse(JSON.stringify(spots))};
+
+  spots.Spots.forEach(spot => {
+    const totalStars = spot.Reviews.reduce((sum, review) => {
+      return sum + review.stars;
+    }, 0);
+    spot.avgRating = totalStars / spot.Reviews.length;
+    delete spot.Reviews;
+
+    spot.SpotImages.forEach(spotImage => {
+      if (spotImage.preview === true) spot.previewImage = spotImage.url;
+    });
+    if (!spot.previewImage) spot.previewImage = 'No preview image';
+    delete spot.SpotImages;
+  });
+
+  return spots;
+}
 
 module.exports = router;
