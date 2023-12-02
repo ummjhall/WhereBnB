97d0e5b3348fc08d6e52cdb4fff78b5b7428f1ce
Justin Hall <justinhall.jnh@gmail.com>
2023-12-02 14:19:44 +0900

    Implement 'authorize' middleware

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const { Spot, SpotImage, Review, User } = require('../../db/models');
-const { requireAuth, isAuthorized } = require('../../utils/auth');
+const { requireAuth, authorize } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 // Get all Spots
-router.get('/', async (req, res, next) => {
+router.get('/', async (_req, res) => {
   let spots = await Spot.findAll({include: [{model: SpotImage}, {model: Review}]});
   spots = formatSpots(spots);
   res.json(spots);
 });
 
 // Get all Spots owned by the Current User
-router.get('/current', requireAuth, async (req, res, next) => {
+router.get('/current', requireAuth, async (req, res) => {
   let ownedSpots = await Spot.findAll({
     where: {ownerId: req.user.id},
     include: [{model: SpotImage}, {model: Review}]
@@ -23,7 +23,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
 });
 
 // Get details of a Spot from an id
-router.get('/:spotId', async (req, res, next) => {
+router.get('/:spotId', async (req, res) => {
   let spot = await Spot.findOne({
     where: {id: req.params.spotId},
     include: [
@@ -47,65 +47,31 @@ router.get('/:spotId', async (req, res, next) => {
 });
 
 const validateSpot = [
-  check('address')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .withMessage('Street address is required'),
-  check('city')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .withMessage('City is required'),
-  check('state')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .withMessage('State is required'),
-  check('country')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .withMessage('Country is required'),
-  check('lat')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .isFloat({min: -90, max: 90})
-    .withMessage('Latitude must be within -90 and 90'),
-  check('lng')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .isFloat({min: -180, max: 180})
-    .withMessage('Longitude must be within -180 and 180'),
-  check('name')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .isLength({max: 50})
-    .withMessage('Name must be less than 50 characters'),
-  check('description')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .withMessage('Description is required'),
-  check('price')
-    .exists({checkFalsy: true})
-    .notEmpty()
-    .isFloat({min: 0})
-    .withMessage('Price per day must be a positive number'),
-    handleValidationErrors
+  check('address').notEmpty().withMessage('Street address is required'),
+  check('city').notEmpty().withMessage('City is required'),
+  check('state').notEmpty().withMessage('State is required'),
+  check('country').notEmpty().withMessage('Country is required'),
+  check('lat').notEmpty().isFloat({min: -90, max: 90}).withMessage('Latitude must be within -90 and 90'),
+  check('lng').notEmpty().isFloat({min: -180, max: 180}).withMessage('Longitude must be within -180 and 180'),
+  check('name').notEmpty().isLength({max: 50}).withMessage('Name must be less than 50 characters'),
+  check('description').notEmpty().withMessage('Description is required'),
+  check('price').notEmpty().isFloat({min: 0}).withMessage('Price per day must be a positive number'),
+  handleValidationErrors
 ];
 
 // Create a Spot
-router.post('/', requireAuth, validateSpot, async (req, res, next) => {
+router.post('/', requireAuth, validateSpot, async (req, res) => {
   const { address, city, state, country, lat, lng, name, description, price } = req.body;
   const newSpot = await Spot.create({ownerId: req.user.id, address, city, state, country, lat, lng, name, description, price});
   res.status(201).json(newSpot);
 });
 
 // Add an Image to a Spot based on the Spot's id
-router.post('/:spotId/images', requireAuth, async (req, res, next) => {
+router.post('/:spotId/images', requireAuth, authorize, async (req, res) => {
   const spotId = req.params.spotId;
   const spot = await Spot.findByPk(spotId);
   if (!spot) {
     return res.status(404).json({message: "Spot couldn't be found"});
-  }
-  if (!await isAuthorized(spotId, req, res)) {
-    return res.status(403).json({message: 'Forbidden'});
   }
 
   const { url, preview } = req.body;
@@ -119,14 +85,10 @@ router.post('/:spotId/images', requireAuth, async (req, res, next) => {
 });
 
 // Edit a Spot
-router.put('/:spotId', requireAuth, validateSpot, async (req, res, next) => {
-  const spotId = req.params.spotId;
-  const spot = await Spot.findByPk(spotId);
+router.put('/:spotId', requireAuth, authorize, validateSpot, async (req, res) => {
+  const spot = await Spot.findByPk(req.params.spotId);
   if (!spot) {
     return res.status(404).json({message: "Spot couldn't be found"});
-  }
-  if (!await isAuthorized(spotId, req, res)) {
-    return res.status(403).json({message: 'Forbidden'});
   }
 
   spot.update(req.body);
@@ -135,14 +97,10 @@ router.put('/:spotId', requireAuth, validateSpot, async (req, res, next) => {
 });
 
 // Delete a Spot
-router.delete('/:spotId', requireAuth, async (req, res, next) => {
-  const spotId = req.params.spotId;
-  const spot = await Spot.findByPk(spotId);
+router.delete('/:spotId', requireAuth, authorize, async (req, res) => {
+  const spot = await Spot.findByPk(req.params.spotId);
   if (!spot) {
     return res.status(404).json({message: "Spot couldn't be found"});
-  }
-  if (!await isAuthorized(spotId, req, res)) {
-    return res.status(403).json({message: 'Forbidden'});
   }
 
   await spot.destroy();
