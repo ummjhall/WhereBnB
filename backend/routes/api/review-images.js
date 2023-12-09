9f36f3f61083ed0eb1b6764727ed6504e6df0de0
Justin Hall <justinhall.jnh@gmail.com>
2023-12-10 03:35:59 +0900

    Implement endpoint: Delete a Review Image

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const { SpotImage } = require('../../db/models');
+const { ReviewImage } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
 router.delete('/:imageId', requireAuth, async (req, res) => {
-  const image = await SpotImage.findByPk(req.params.imageId);
-  if (!image) return res.status(404).json({"message": "Spot Image couldn't be found"});
+  const image = await ReviewImage.findByPk(req.params.imageId);
+  if (!image) return res.status(404).json({"message": "Review Image couldn't be found"});
 
-  const spotOwnerId = await image.getSpot().then(res => res.ownerId);
-  if (req.user.id != spotOwnerId) {
+  const reviewOwnerId = await image.getReview().then(res => res.userId);
+  if (req.user.id != reviewOwnerId) {
     return res.status(403).json({"message": "Forbidden"});
   }
 
