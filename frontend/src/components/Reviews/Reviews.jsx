6154c6acf406a4a118de600cbb19461ebb1b7c72
Justin Hall <justinhall.jnh@gmail.com>
2024-01-14 07:28:41 +0900

    Reviews: Render spot reviews with the correct date format

--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -6,8 +6,9 @@ function Reviews({ spot }) {
   const spotReviews = useSelector(state => state.reviews[spot.id]?.spotReviews);
   let reviewsArray;
   if (spotReviews) reviewsArray = Object.values(spotReviews);
-
   const dispatch = useDispatch();
+  const months = ['January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'];
 
   useEffect(() => {
     dispatch(getSpotReviews(spot.id));
@@ -20,9 +21,15 @@ function Reviews({ spot }) {
       <div>
         {reviewsArray && reviewsArray.map(review => (
           <div key={review.id}>
-            <div>{review.User.firstName}</div>
-            <div>{review.createdAt}</div>
-            <div>{review.review}</div>
+            <div>
+              {review.User.firstName}
+            </div>
+            <div>
+              {`${months[new Date(review.createdAt).getMonth()]} ${review.createdAt.slice(0, 4)}`}
+            </div>
+            <div>
+              {review.review}
+            </div>
           </div>
         ))}
       </div>
