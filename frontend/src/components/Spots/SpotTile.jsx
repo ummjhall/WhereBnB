461dcb11719bc2e3dfed8ab90390c3941a280714
Justin Hall <justinhall.jnh@gmail.com>
2024-01-14 01:00:47 +0900

    SpotTile: Change image from placeholder to previewImage

--- frontend/src/components/Spots/SpotTile.jsx
+++ frontend/src/components/Spots/SpotTile.jsx
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
-import jh from '../../../../images/jh.jpg';
 import './Spots.css';
 
 function SpotTile({ spot }) {
   return (
     <Link to={`spots/${spot.id}`} className='spot-tile tooltip'>
       <span className='tooltip-text'>{spot.name}</span>
-      <img className='spot-tile-img' src={jh} />
+      <img className='spot-tile-img' src={spot.previewImage} />
       <div className='spot-tile_info-span'>
         <span>{spot.city}, {spot.state}</span>
         <span>★ {!spot.avgRating ? 'New' : spot.avgRating.toFixed(1)}</span>
